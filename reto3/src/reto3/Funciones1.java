4df222d2062ddf5ec8f9441e259f7d241db7000c
GRDinculescu <grdinculescu@gmail.com>
2024-11-14 10:06:42 +0100

    Arreglado Funciones1.java

--- reto3/src/reto3/Funciones1.java
+++ reto3/src/reto3/Funciones1.java
@@ -15,25 +15,65 @@ public class Funciones1 {
 		diferenciaMeses(fecha1, fecha2, sc);
 	}
 	*/
-	public static void diferenciaMeses(LocalDate fecha1, LocalDate fecha2, Scanner sc) {
-		int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
-		System.out.println("La diferencia de meses entre las dos fechas es:" + meses);
-		if (meses < 3) {
-			for (int i = 0; i <= 4; i++) {
-				System.out.println("Introduce" + i + "." + "numero por pantalla:");
-				String resultado1= sc.nextLine();
-			}
-		} else {
-			System.out.println("Introduce " + meses + "nombres por consola:");
-			String resultado2= sc.nextLine();
-		}
 
-	}
-	public static void tamañoMasPequeño( String resultado1, String resultado2, Scanner sc){
-		String pequeño;
-		String s = null;
-		int len = s.length();
-		
+	public static void diferenciaMeses(LocalDate fecha1, LocalDate fecha2, Scanner sc) {
+		if (fecha1.getMonth() != fecha2.getMonth()) {
+			int meses = (int) ChronoUnit.MONTHS.between(fecha1, fecha2);
+
+			int longitud = 0;
+			String nombre = "";
+			String nombreCorto = "";
+			boolean primerNombre = true;
+
+
+
+			if (meses > 3) {
+
+				for (int i = 0; i < meses; i++) {
+					System.out.println("Insera nombre: ");
+					nombre = sc.nextLine();
+					if (primerNombre) {
+						longitud = nombre.length();
+						primerNombre = false;
+					}
+
+					if (nombre.length() < longitud) {
+						longitud = nombre.length();
+						nombreCorto = nombre;
+					}
+				}
+
+				System.out.println("El nombre mas corto es: "+nombreCorto);
+			} else {
+				for (int i = 0; i < 4; i++) {
+					System.out.println("Insera nombre: ");
+					nombre = sc.nextLine();
+					if (primerNombre) {
+						longitud = nombre.length();
+						primerNombre = false;
+					}
+
+					if (nombre.length() < longitud) {
+						longitud = nombre.length();
+						nombreCorto = nombre;
+					}
+				}
+
+				System.out.println("El nombre mas corto es: "+nombreCorto);
+			}
+
+			for (int i = 0; i < nombre.length(); i++) {
+				switch (nombre.charAt(i)) {
+					case 'a','e','i','o','u' -> {
+						if (i == nombre.length() -1) {
+							System.out.print(nombre.charAt(i));
+						} else {
+							System.out.print(nombre.charAt(i)+"-");
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
